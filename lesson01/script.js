72c9df47427ec53b209256803be12d5e89135087
Ivan Krashevskiy <xxxserafimxxx@ukr.net>
2021-01-29 20:33:40 +0200

    Сделал домашку

---
+++ lesson01/script.js
@@ -0,0 +1,9 @@
+let money = 'are needed'; 
+let income = 'insignificant'; 
+let addExpenses = 'insignificant'; 
+let deposit = 'No'; 
+let mission = 'Junior'; 
+let period = 1 + 'Years';
+
+alert('Hello JS');
+console.log('hi');
