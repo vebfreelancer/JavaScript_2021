012a0712dcee8d83be672b76013d365b430b328c
Ivan Krashevskiy <xxxserafimxxx@ukr.net>
2021-01-30 00:28:46 +0200

    DZ02

---
+++ lesson02/script.js
@@ -0,0 +1,17 @@
+let money = 20000;
+let income = 5000;
+let addExpenses = 'Развлечения, собственные нужды, отдых';
+let deposit = 10 > 5;
+let mission = 1000000;
+let period = 6;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев. ' + 'Цель заработать ' + mission + ' гривен.');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(   ));
+
+let budgetDay = 1000;
+console.log(budgetDay);
