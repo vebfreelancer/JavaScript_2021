04a5723d8a30b55ef0629495d515e2e0a4ece482
vebfreelancer <70976918+vebfreelancer@users.noreply.github.com>
2021-02-02 22:38:40 +0200

    Update script.js
    Правки_02

--- lesson 03/script.js
+++ lesson 03/script.js
@@ -2,72 +2,46 @@
 
 // Обьявляем переменные
 let money = +prompt('Ваш месячный доход');
+// console.log(`Ваш месячный доход ${money}`);
 
-let addExpenses = +prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Квартплата, проездной, кредит');
-// Добавляем условие если пользователь вводит текст ему выводится оповещение
-// что необходимо ввести данные в цифром формате, после повторяем запрос ввода
-if (addExpenses === Number) {
-    addExpenses = true;
-} else if (addExpenses !== Number) {
-    confirm('Данные нужно ввести в числовом формате!');
-    addExpenses = +prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Квартплата, проездной, кредит');
-}
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Квартплата, проездной, кредит');
+console.log(addExpenses.split(','));
 
 let deposit = confirm('Есть ли у вас депозит в банке?');
-let mission = 1000000;
-let budgetDay = 30000 / 30;
-// console.log(money);
-// console.log(addExpenses);
 // console.log(deposit);
 
-let qestions = 1;
-let expenses1;
-let expenses2;
-let amount1;
-let amount2;
+let mission = 1000000;
 
-// Используя switch решаем серию вопросов
-switch(qestions) {
-    case 1:
-        expenses1 = prompt('Введите обязательную статью расходов?');
-        // console.log(expenses1);
-    case 2:
-        amount1 = +prompt('Во сколько это обойдется?');
-        // console.log(amount1);
-    case 3:
-        expenses2 = prompt('Введите обязательную статью расходов?');
-        // console.log(expenses2);
-    case 4:
-        amount2 = +prompt('Во сколько это обойдется?');
-        // console.log(amount2);
-}
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
+let amount1 = +prompt('Во сколько это обойдется?');
+let amount2 = +prompt('Во сколько это обойдется?');
 
-// Сумируем расходы получаем месячный бюджет
-let budgetMonth = addExpenses + amount1 + amount2;
-console.log(budgetMonth);
+// Вычисляем месячный бюджет
+let budgetMonth = money - (amount1 + amount2);
+// console.log('budgetMonth: ' + budgetMonth);
 
-// Берем разницу месячных расходов и доходов делим
-// на значение миссии выводим необходимое время
-mission /= (money - budgetMonth);
-console.log(Math.round(mission));
+// Вычисляем время выполнения мисси
+mission /= budgetMonth;
+console.log('Время mission: ' + Math.round(mission));
 
 // Делим месячный бюджет на 30 дней выводим дневной бюджет
-budgetDay = budgetMonth / 30;
-console.log(budgetDay);
+let budgetDay = budgetMonth / 30;
+console.log('budgetDay:' + Math.floor(budgetDay));
 
 // Используя ветку условий определяем уровень доходов
 if (budgetDay > 1200) {
     console.log('У вас высокий уровень дохода');
-}   else if (budgetDay > 600 && budgetDay < 1200) {
+} else if (budgetDay > 600 && budgetDay < 1200) {
     console.log('У вас средний уровень дохода');
-}   else if (budgetDay < 600 && budgetDay > 0) {
+} else if (budgetDay < 600 && budgetDay > 0) {
     console.log('К сожалению у вас уровень дохода ниже среднего');
-}   else if (budgetDay < 0) {
+} else if (budgetDay < 0) {
     console.log('Что то пошло не так');
-}   else if (budgetDay = 0) {
+} else if (budgetDay = 0) {
     console.log('У вас нулевой уровень дохода');
-}   else if (budgetDay = 600) {
+} else if (budgetDay = 600) {
     console.log('У вас нормальный уровень дохода');
-}   else if (budgetDay = 1200) {
+} else if (budgetDay = 1200) {
     console.log('У вас неплохой уровень дохода');
 }
