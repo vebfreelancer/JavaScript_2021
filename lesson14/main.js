4560891d4a9a8e0a8f1dbbd1f66db695d10eb245
vebfreelancer <70976918+vebfreelancer@users.noreply.github.com>
2021-02-24 09:23:01 +0200

    Update main.js
    Внес правки

--- lesson14/main.js
+++ lesson14/main.js
@@ -1,8 +1,7 @@
-'use strict';
-
 let body = document.querySelector('body');
-//класс
-function DomElement(selector, options){
+//класс 
+class DomElement {
+    constructor(selector, options){
     this.selector = selector;
     options = options || {},
     this.height = options.height;
@@ -10,35 +9,33 @@ function DomElement(selector, options){
     this.bg = options.bg;
     this.fontSize = options.fontSize;
 }
-
-DomElement.prototype.render =  function(){
-    let count = this.selector.slice(0, 1);  
-    if(count === '.'){
-        // console.log(this.selector.slice(0, 1));
-        this.newElement('div');  
-    }else if(count === '#'){
-        this.newElement('p');  
+   NewElement(n){
+        console.log(n);
+        let count = n.slice(0, 1);  
+            if(count === '.'){
+            let div = document.createElement('div');
+            div.setAttribute('contenteditable', 'true');
+            div.classList.add(`${n}`.slice(1)); 
+            this.render (div);
+        } else if(count === '#') {
+            let paragraph = document.createElement('p');
+            paragraph.id = ${n}.slice(1); 
+            paragraph.setAttribute('contenteditable', 'true');
+            this.render (paragraph);
+        }
     }
-};
-
-DomElement.prototype.newElement =  function(value){
-    // console.log(value);
-    let el = document.createElement(`${value}`);
-        el.setAttribute('contenteditable', 'true');
-        el.classList.add(this.selector.slice(1));  
-        el.style.height = `${this.height}`;
-        el.style.width = `${this.width}`;
-        el.style.backgroundColor = `${this.bg}`;
-        el.style.fontSize = `${this.fontSize}`;
-        body.append(el); 
-};
-
+    render (value){
+        value.style.height = ${this.height};
+        value.style.width = ${this.width};
+        value.style.backgroundColor = ${this.bg};
+        value.style.fontSize = ${this.fontSize};
+        body.append(value); 
+    }
+}
 //options передаются отдельным объектом
-let Square = new DomElement('.block',{height: '50px', width: '300px', bg: '#89899e', fontSize: '25px'});
-// console.log(Square);
-
-Square.render();
-// console.log(Square instanceof DomElement);
+let Square = new DomElement('selector',{height: '50px', width: '300px', bg: '#89899e', fontSize: '25px'});
+console.log(Square);
+Square.NewElement('#paragraph');
 
 
 
